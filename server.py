2be2d2ee4bb8574bf9ef8993c389730ea7015d44
Omar Elleboudy <omarelleboudy@gmail.com>
2020-02-26 15:49:45 +0200

    Changed port

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
-server_address = ('0.0.0.0', 5678)
+server_address = ('0.0.0.0', 4467)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
