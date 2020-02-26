4726aab356f1c5513f876e9a955559594e639e27
Omar Elleboudy <omarelleboudy@gmail.com>
2020-02-26 19:29:42 +0200

    TCP multi thread

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket,os
-
+# import thread module 
+from thread import *
+import threading 
 # Create a TCP/IP socket
 sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 
@@ -10,15 +12,62 @@ sockTCP.listen(5)
 
 CONNECTION_LIST = []
 
-while True:  
-    print("Waiting for TCP Message...")
-    connection,address = sockTCP.accept()  
-    print("Client " + str(address) + " connected.")
-    CONNECTION_LIST.append(connection)
-    buf = connection.recv(1024)  
-    print ("Got message: " + str(buf))
-    
-    print("Looping over " + str(len(CONNECTION_LIST)) + " connected clients..")
+print_lock = threading.Lock() 
+  
+# thread function 
+def threaded(c): 
+    while True: 
+  
+        # data received from client 
+        data = c.recv(1024) 
+        if not data: 
+            print('Bye') 
+              
+            # lock released on exit 
+            print_lock.release() 
+            break
 
-    for con in CONNECTION_LIST:
-        con.send(buf)
+        # send back string to all client 
+        print("Broadcasting to all clients, which are: " + str(len(CONNECTION_LIST)))
+        for conn in CONNECTION_LIST:
+            conn.send(data)
+  
+    # connection closed 
+    c.close() 
+
+def Main(): 
+    host = "0.0.0.0" 
+  
+    # reverse a port on your computer 
+    # in our case it is 4466 but it 
+    # can be anything 
+    port = 4466
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    s.bind((host, port)) 
+    print("socket binded to port", port) 
+  
+    # put the socket into listening mode 
+    s.listen(5) 
+    print("socket is listening") 
+  
+    # a forever loop until client wants to exit 
+    while True: 
+  
+        # establish connection with client 
+        c, addr = s.accept() 
+
+        print("Client connected with address " + str(addr))
+
+        CONNECTION_LIST.append(c)
+  
+        # lock acquired by client 
+        print_lock.acquire() 
+        print('Connected to :', addr[0], ':', addr[1]) 
+  
+        # Start a new thread and return its identifier 
+        start_new_thread(threaded, (c,)) 
+    s.close() 
+  
+  
+if __name__ == '__main__': 
+    Main()
